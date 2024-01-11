c87db4d5609d0b7af30597240d3d3f785d7be077
Brilliano Dhiya Ulhaq <brillidhiya@gmail.com>
2024-01-12 00:44:56 +0700

    proteksi

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,12 @@
 import DashboardLayout from "@dsarea/@/components/layout/DashboardLayout";
+import { HasDsAreaCookie } from "@dsarea/@/lib/DsAreaCookies";
+import { redirect } from "next/navigation";
 import React, { ReactNode } from "react";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default async function Layout({ children }: { children: ReactNode }) {
+  const hasCookie = await HasDsAreaCookie();
+  if (!hasCookie) {
+    redirect("/");
+  }
   return <DashboardLayout>{children}</DashboardLayout>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 import type { Metadata } from "next";
+import { HasDsAreaCookie } from "@dsarea/@/lib/DsAreaCookies";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Dsarea | Masuk",
   description: "Masuk untuk melanjutkan",
 };
 
-export default function Home() {
+export default async function Home() {
+  const hasCookie = await HasDsAreaCookie();
+
+  if (hasCookie) {
+    redirect("/dashboard");
+  }
   return (
     <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-[#5acacc]">
       <div className="max-w-md w-full space-y-8">
@@ -40,4 +47,4 @@ export default function Home() {
   );
 }
 // https://dsarea-tryout.vercel.app/auth-success?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZV9pZCI6MSwidXNlcl9pZCI6IjEwMzYxNTgyMDA3NjMzMjM4MDk5MyIsImVtYWlsIjoiZGlhbmNhbmRyYTExMkBnbWFpbC5jb20iLCJuYW1lIjoiRElBTiIsImlhdCI6MTcwNDc2ODQ0OSwiZXhwIjoxNzA0NzcyMDQ5fQ._QxiwouOzlfi15vhxIL2QyPdoO1M1JEVigQiM6mYmK8
-// https://dsarea-tryout.vercel.app/auth-success?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Miwicm9sZV9pZCI6MywidXNlcl9pZCI6IjEwMDU3NDMyOTAwNDQ4NzI4MDQ2MyIsImVtYWlsIjoiYnJpbGxpZGhpeWFAZ21haWwuY29tIiwibmFtZSI6IkJyaWxsaWFubyBEaGl5YSBVbGhhcSIsImlhdCI6MTcwNDk5MjQ1MiwiZXhwIjoxNzM2MDk2NDUyfQ.uISTOZt3KywaHBscjOTtqxY8_qVUPl11mlni4Arym0A
+// http://localhost:3000/auth-success?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Miwicm9sZV9pZCI6MywidXNlcl9pZCI6IjEwMDU3NDMyOTAwNDQ4NzI4MDQ2MyIsImVtYWlsIjoiYnJpbGxpZGhpeWFAZ21haWwuY29tIiwibmFtZSI6IkJyaWxsaWFubyBEaGl5YSBVbGhhcSIsImlhdCI6MTcwNDk5MjQ1MiwiZXhwIjoxNzM2MDk2NDUyfQ.uISTOZt3KywaHBscjOTtqxY8_qVUPl11mlni4Arym0A
