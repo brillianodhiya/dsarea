586eae52b70eb64f41969d9bd4e5e3732d1832a0
Brilliano Dhiya Ulhaq <brillidhiya@gmail.com>
2024-01-14 01:52:55 +0700

    role

--- app/(dashboard)/role/page.tsx
+++ app/(dashboard)/role/page.tsx
@@ -7,7 +7,6 @@ import {
   SearchOutlined,
 } from "@ant-design/icons";
 import DropdownMenu from "@dsarea/@/components/Dropdown/DropdownMenu";
-import AddCategoryModal from "@dsarea/@/components/Modals/AddCategoryModal";
 import AddVourcherModal from "@dsarea/@/components/Modals/AddVoucherModal";
 import CustomHeader from "@dsarea/@/components/layout/CustomeHeader";
 import { axiosClientInstance } from "@dsarea/@/lib/AxiosClientConfig";
