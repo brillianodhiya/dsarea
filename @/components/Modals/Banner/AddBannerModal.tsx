ed505c60b8111908e78740576cb4c5eca1aae885
Rayha <taqiudin.ahyari@gmail.com>
2024-01-16 09:19:36 +0700

    add banner

---
+++ @/components/Modals/Banner/AddBannerModal.tsx
@@ -0,0 +1,215 @@
+import React from "react";
+import { Form, Input, Modal, Space, Switch, Upload, message } from "antd";
+import { Spin } from "antd/lib";
+import type { UploadProps } from "antd";
+import {
+  CloudUploadOutlined,
+  LoadingOutlined,
+  PlusOutlined,
+} from "@ant-design/icons";
+import Image from "next/image";
+interface Values {
+  image: any;
+  title: string;
+  description: string;
+  modifier: string;
+}
+
+interface AddBannerModalProps {
+  open: boolean;
+  onCreate: (values: Values) => void;
+  onCancel: () => void;
+  loading: boolean;
+}
+// type FileType = Parameters<GetProp<UploadProps, "beforeUpload">>[0];
+
+const getBase64 = (img: any, callback: (url: string) => void) => {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => callback(reader.result as string));
+  reader.readAsDataURL(img);
+};
+
+const beforeUpload = (file: any) => {
+  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+  if (!isJpgOrPng) {
+    message.error("You can only upload JPG/PNG file!");
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error("Image must smaller than 2MB!");
+  }
+  return isJpgOrPng && isLt2M;
+};
+const AddBannerModal: React.FC<AddBannerModalProps> = ({
+  open,
+  onCreate,
+  onCancel,
+  loading,
+}) => {
+  const [form] = Form.useForm();
+  const [checked, setChecked] = React.useState(true);
+  const [imageUrl, setImageUrl] = React.useState<string>();
+  const [imageLoading, setImageLoading] = React.useState(false);
+
+  const dummyRequest = ({ file, onSuccess }: any) => {
+    setTimeout(() => {
+      onSuccess("ok");
+    }, 0);
+  };
+
+  const props: UploadProps = {
+    name: "file",
+    multiple: false,
+    showUploadList: false,
+    customRequest: dummyRequest,
+
+    onChange(info) {
+      const { status } = info.file;
+      if (status !== "uploading") {
+        console.log(info.file, info.fileList);
+        setImageLoading(true);
+      }
+      if (status === "done") {
+        message.success(`${info.file.name} file uploaded successfully.`);
+        getBase64(info.file.originFileObj, (url) => {
+          setImageUrl(url);
+          setImageLoading(false);
+        });
+      } else if (status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+    onDrop(e) {
+      console.log("Dropped files", e.dataTransfer.files);
+    },
+    beforeUpload(file) {
+      const isJpgOrPng =
+        file.type === "image/jpeg" || file.type === "image/png";
+      if (!isJpgOrPng) {
+        message.error("You can only upload JPG/PNG file!");
+      }
+      const isLt2M = file.size / 1024 / 1024 < 2;
+      if (!isLt2M) {
+        message.error("Image must smaller than 2MB!");
+      }
+      return isJpgOrPng && isLt2M;
+    },
+  };
+
+  return (
+    <Modal
+      open={open}
+      title="Unggah Banner"
+      okText="Save"
+      cancelText="Cancel"
+      onCancel={() => {
+        setImageUrl(undefined);
+        onCancel();
+        setChecked(true);
+      }}
+      onOk={() => {
+        form
+          .validateFields()
+          .then((values) => {
+            form.resetFields();
+            onCreate(values);
+          })
+          .catch((info) => {
+            // console.log("Validate Failed:", info);
+          });
+        setImageUrl(undefined);
+        setChecked(true);
+      }}
+    >
+      <Spin spinning={loading}>
+        <Form
+          form={form}
+          layout="vertical"
+          name="form_in_modal"
+          initialValues={{ modifier: "public" }}
+        >
+          <Form.Item
+            name={"image"}
+            rules={[
+              {
+                required: true,
+                message: "Gambar tidak boleh kosong!",
+              },
+            ]}
+          >
+            <Upload.Dragger {...props}>
+              {imageUrl ? (
+                <Image
+                  src={imageUrl}
+                  alt="banner image"
+                  width={20}
+                  height={20}
+                  style={{
+                    width: "100%",
+                    height: 100,
+                    objectFit: "contain",
+                  }}
+                />
+              ) : (
+                <>
+                  {imageLoading ? (
+                    <LoadingOutlined />
+                  ) : (
+                    <>
+                      <CloudUploadOutlined
+                        style={{
+                          fontSize: 44,
+                          color: "#667085",
+                        }}
+                      />
+                      <p className="ant-upload-text">Click to upload</p>
+                      <p className="ant-upload-hint">png atau jpeg</p>
+                    </>
+                  )}
+                </>
+              )}
+            </Upload.Dragger>
+          </Form.Item>
+
+          <Form.Item
+            valuePropName="checked"
+            initialValue={true}
+            name="status"
+            label="Status"
+          >
+            <Space>
+              {checked ? "Active" : "No Active"}
+              <Switch
+                defaultChecked={true}
+                onChange={(checked) => {
+                  setChecked(checked);
+                  form.setFieldValue("status", checked);
+                }}
+                style={{
+                  backgroundColor: checked ? "#3A9699" : "#E0E0E0",
+                }}
+              />
+            </Space>
+          </Form.Item>
+          <Form.Item
+            rules={[
+              {
+                required: true,
+                message: "Judul tidak boleh kosong!",
+              },
+            ]}
+            name="title"
+            label="Judul"
+          >
+            <Input placeholder="Banner Name" />
+          </Form.Item>
+
+          <Form.Item name="description" label="Description">
+            <Input.TextArea rows={2} placeholder="Description" />
+          </Form.Item>
+        </Form>
+      </Spin>
+    </Modal>
+  );
+};
+export default AddBannerModal;
--- app/(dashboard)/banner/page.tsx
+++ app/(dashboard)/banner/page.tsx
@@ -1,5 +1,152 @@
+"use client";
+import { PlusOutlined, SearchOutlined } from "@ant-design/icons";
+import AddBannerModal from "@dsarea/@/components/Modals/Banner/AddBannerModal";
+import CustomHeader from "@dsarea/@/components/layout/CustomeHeader";
+import { axiosClientInstance } from "@dsarea/@/lib/AxiosClientConfig";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { Card, Col, Input, Row, Space, Tag, Typography, message } from "antd";
+import Meta from "antd/es/card/Meta";
 import Button from "antd/lib/button";
+import Image from "next/image";
+import React from "react";
 
 export default function Page() {
-  return <Button type="primary">TEST</Button>;
+  const [openAddModal, setOpenAddModal] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const [countFetch, setCountFetch] = React.useState(0);
+  const queryClient = useQueryClient();
+
+  const { data, isFetching } = useQuery({
+    queryKey: ["Banner"],
+    queryFn: async () => {
+      const res = await axiosClientInstance.get("/api/banner/list");
+      return res.data.data;
+    },
+    initialData: [
+      {
+        id: 0,
+        name: "test",
+        desc: "test",
+        image: "/card-image.svg",
+      },
+    ],
+  });
+
+  console.log(data);
+
+  return (
+    <div>
+      <AddBannerModal
+        open={openAddModal}
+        onCreate={async (values) => {
+          const formData = new FormData();
+          formData.append("title", values.title);
+          formData.append("desc", values.description);
+          formData.append("image", values.image);
+          try {
+            setLoading(true);
+            const res = await axiosClientInstance.post(
+              "/api/banner/create",
+              formData,
+              {
+                headers: {
+                  "Content-Type": "multipart/form-data",
+                },
+              }
+            );
+            queryClient.invalidateQueries({
+              queryKey: ["Banner"],
+            });
+            setLoading(false);
+            message.success(`${res.data.message}`);
+            setOpenAddModal(false);
+          } catch (error) {
+            console.log(error);
+            setLoading(false);
+            message.error(
+              `${(error as any).response.data.message} : ${
+                (error as any).response.data
+              }`
+            );
+          }
+          console.log(values);
+        }}
+        onCancel={() => {
+          setOpenAddModal(false);
+        }}
+        loading={false}
+      />
+      <CustomHeader title="Banner" />
+
+      <Card className="!m-6">
+        <Row className="mb-4" justify={"space-between"} wrap>
+          <Col>
+            <Typography.Text strong className="!text-xl">
+              List Banner
+            </Typography.Text>
+          </Col>
+          <Col>
+            <Space wrap>
+              <Input
+                placeholder="Search anything..."
+                suffix={<SearchOutlined />}
+                className="!w-[250px]"
+              />
+              <Button
+                type="primary"
+                onClick={() => setOpenAddModal(true)}
+                color="red"
+                icon={<PlusOutlined />}
+              >
+                Unggah Banner
+              </Button>
+            </Space>
+          </Col>
+        </Row>
+
+        {data.map((e: any, i: any) => (
+          <Card
+            style={{
+              marginTop: 20,
+            }}
+            loading={isFetching}
+            key={i}
+            bodyStyle={{
+              padding: 0,
+              margin: 0,
+              flexDirection: "row",
+              display: "flex",
+              alignItems: "center",
+              flexWrap: "wrap",
+            }}
+          >
+            <Image
+              alt="example"
+              src={"/card-image.svg"}
+              width={240}
+              height={100}
+            />
+
+            <div className="flex-row p-4 ">
+              {/* <Typography.Text strong>{e.title}</Typography.Text>
+              <Typography.Paragraph>{e.desc}</Typography.Paragraph> */}
+              <Meta title={e.title} description={e.desc} />
+            </div>
+
+            <Tag
+              style={{
+                position: "absolute",
+                zIndex: 1,
+                right: "10px",
+                borderRadius: "10px",
+              }}
+              color={e.status ? "#32D583" : "#F04438"}
+            >
+              {e.status ? "Active" : "Inactive"}
+            </Tag>
+          </Card>
+        ))}
+      </Card>
+    </div>
+  );
 }
