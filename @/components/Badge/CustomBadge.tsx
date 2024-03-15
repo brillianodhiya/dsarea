1bad511ba9338c78ff1217d6622909f708109909
Brilliano Dhiya Ulhaq <brillidhiya@gmail.com>
2024-03-15 11:10:20 +0700

    custom badge

--- @/components/Badge/CustomBadge.tsx
+++ @/components/Badge/CustomBadge.tsx
@@ -14,34 +14,37 @@ export const CustomBadge: React.FC<BadgeProps> = ({ value, status }) => {
   return (
     <>
       {status === "success" ? (
-        <Typography className="capitalize">
+        <Typography
+          className="capitalize"
+          style={{
+            color: "#32D583",
+            fontSize: "10px",
+          }}
+        >
           {/* Type error: Type '{ className: string; }' is missing the following properties from type 'Pick<AntdIconProps, "prefix" | "media" | "data" | "height" | "width" | "translate" | "rotate" | "content" | "muted" | "headers" | "method" | "size" | "value" | "name" | ... 349 more ... | "twoToneColor">': onPointerEnterCapture, onPointerLeaveCapture */}
-          <CheckCircleFilled
-            style={{
-              color: "#32D583",
-              fontSize: "10px",
-            }}
-          />
+          <CheckCircleFilled />
           {" " + value}
         </Typography>
       ) : status === "pending" ? (
-        <Typography className="capitalize">
-          <ExclamationCircleFilled
-            style={{
-              color: "#FDB022",
-              fontSize: "10px",
-            }}
-          />
+        <Typography
+          className="capitalize"
+          style={{
+            color: "#FDB022",
+            fontSize: "10px",
+          }}
+        >
+          <ExclamationCircleFilled />
           {" " + value}
         </Typography>
       ) : (
-        <Typography className="capitalize">
-          <MinusCircleFilled
-            style={{
-              color: "#F04438",
-              fontSize: "10px",
-            }}
-          />
+        <Typography
+          className="capitalize"
+          style={{
+            color: "#F04438",
+            fontSize: "10px",
+          }}
+        >
+          <MinusCircleFilled />
           {" " + value}
         </Typography>
       )}
